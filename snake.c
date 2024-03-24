fa5eecb7a1b2e9fb6c39b5d007689382ca6d4cc8
Anish Bhaduri <130269010+Anishbhaduri@users.noreply.github.com>
2024-03-24 21:59:17 +0530

    snake.c

--- snake.c
+++ snake.c
@@ -1,148 +1,222 @@
-// C program to build the complete 
-// snake game 
-#include <conio.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+#include <ncurses.h>
 
-int i, j, height = 20, width = 20; 
-int gameover, score; 
-int x, y, fruitx, fruity, flag; 
+#define WIDTH 50
+#define HEIGHT 20
+#define INITIAL_LENGTH 5
+#define SPEED 150000
 
-// Function to generate the fruit 
-// within the boundary 
-void setup() 
-{ 
-	gameover = 0; 
+typedef struct {
+    int x;
+    int y;
+} Point;
 
-	// Stores height and width 
-	x = height / 2; 
-	y = width / 2; 
-label1: 
-	fruitx = rand() % 20; 
-	if (fruitx == 0) 
-		goto label1; 
-label2: 
-	fruity = rand() % 20; 
-	if (fruity == 0) 
-		goto label2; 
-	score = 0; 
-} 
+typedef enum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direction;
 
-// Function to draw the boundaries 
-void draw() 
-{ 
-	system("cls"); 
-	for (i = 0; i < height; i++) { 
-		for (j = 0; j < width; j++) { 
-			if (i == 0 || i == width - 1 
-				|| j == 0 
-				|| j == height - 1) { 
-				printf("#"); 
-			} 
-			else { 
-				if (i == x && j == y) 
-					printf("0"); 
-				else if (i == fruitx 
-						&& j == fruity) 
-					printf("*"); 
-				else
-					printf(" "); 
-			} 
-		} 
-		printf("\n"); 
-	} 
+typedef struct {
+    Point head;
+    Point body[WIDTH * HEIGHT];
+    Direction direction;
+    int length;
+} Snake;
 
-	// Print the score after the 
-	// game ends 
-	printf("score = %d", score); 
-	printf("\n"); 
-	printf("press X to quit the game"); 
-} 
+typedef struct {
+    Point position;
+    bool active;
+} Food;
 
-// Function to take the input 
-void input() 
-{ 
-	if (kbhit()) { 
-		switch (getch()) { 
-		case 'a': 
-			flag = 1; 
-			break; 
-		case 's': 
-			flag = 2; 
-			break; 
-		case 'd': 
-			flag = 3; 
-			break; 
-		case 'w': 
-			flag = 4; 
-			break; 
-		case 'x': 
-			gameover = 1; 
-			break; 
-		} 
-	} 
-} 
+void setup();
+void draw();
+void input();
+void logic();
+void spawn_food();
+bool collision(Point p);
+void game_over();
+void cleanup();
 
-// Function for the logic behind 
-// each movement 
-void logic() 
-{ 
-	sleep(0.01); 
-	switch (flag) { 
-	case 1: 
-		y--; 
-		break; 
-	case 2: 
-		x++; 
-		break; 
-	case 3: 
-		y++; 
-		break; 
-	case 4: 
-		x--; 
-		break; 
-	default: 
-		break; 
-	} 
+Snake snake;
+Food food;
+bool running = true;
 
-	// If the game is over 
-	if (x < 0 || x > height 
-		|| y < 0 || y > width) 
-		gameover = 1; 
+int main() {
+    setup();
 
-	// If snake reaches the fruit 
-	// then update the score 
-	if (x == fruitx && y == fruity) { 
-	label3: 
-		fruitx = rand() % 20; 
-		if (fruitx == 0) 
-			goto label3; 
+    while (running) {
+        draw();
+        input();
+        logic();
+        usleep(SPEED);
+    }
 
-	// After eating the above fruit 
-	// generate new fruit 
-	label4: 
-		fruity = rand() % 20; 
-		if (fruity == 0) 
-			goto label4; 
-		score += 10; 
-	} 
-} 
+    cleanup();
+    return 0;
+}
 
-// Driver Code 
-void main() 
-{ 
-	int m, n; 
+void setup() {
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    timeout(0);
 
-	// Generate boundary 
-	setup(); 
+    snake.head.x = WIDTH / 2;
+    snake.head.y = HEIGHT / 2;
+    snake.direction = RIGHT;
+    snake.length = INITIAL_LENGTH;
 
-	// Until the game is over 
-	while (!gameover) { 
+    for (int i = 0; i < snake.length; i++) {
+        snake.body[i].x = snake.head.x - i;
+        snake.body[i].y = snake.head.y;
+    }
 
-		// Function Call 
-		draw(); 
-		input(); 
-		logic(); 
-	} 
-} 
+    spawn_food();
+}
+
+void draw() {
+    clear();
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) {
+                mvprintw(i, j, "#");
+            } else if (i == food.position.y && j == food.position.x && food.active) {
+                mvprintw(i, j, "F");
+            } else {
+                bool is_snake = false;
+
+                for (int k = 0; k < snake.length; k++) {
+                    if (snake.body[k].x == j && snake.body[k].y == i) {
+                        mvprintw(i, j, "O");
+                        is_snake = true;
+                        break;
+                    }
+                }
+
+                if (!is_snake) {
+                    mvprintw(i, j, " ");
+                }
+            }
+        }
+    }
+
+    refresh();
+}
+
+void input() {
+    int key = getch();
+
+    switch (key) {
+        case KEY_UP:
+            if (snake.direction != DOWN) {
+                snake.direction = UP;
+            }
+            break;
+        case KEY_DOWN:
+            if (snake.direction != UP) {
+                snake.direction = DOWN;
+            }
+            break;
+        case KEY_LEFT:
+            if (snake.direction != RIGHT) {
+                snake.direction = LEFT;
+            }
+            break;
+        case KEY_RIGHT:
+            if (snake.direction != LEFT) {
+                snake.direction = RIGHT;
+            }
+            break;
+        case 'q':
+            running = false;
+            break;
+    }
+}
+
+void logic() {
+    Point prev = snake.body[0];
+    Point temp;
+
+    snake.body[0] = snake.head;
+
+    for (int i = 1; i < snake.length; i++) {
+        temp = snake.body[i];
+        snake.body[i] = prev;
+        prev = temp;
+    }
+
+    switch (snake.direction) {
+        case UP:
+            snake.head.y--;
+            break;
+        case DOWN:
+            snake.head.y++;
+            break;
+        case LEFT:
+            snake.head.x--;
+            break;
+        case RIGHT:
+            snake.head.x++;
+            break;
+    }
+
+    if (collision(snake.head)) {
+        game_over();
+        return;
+    }
+
+    if (snake.head.x == food.position.x && snake.head.y == food.position.y && food.active) {
+        snake.length++;
+        spawn_food();
+    }
+}
+
+void spawn_food() {
+    srand(time(NULL));
+    int x, y;
+
+    do {
+        x = rand() % (WIDTH - 2) + 1;
+        y = rand() % (HEIGHT - 2) + 1;
+    } while (collision((Point){x, y}));
+
+    food.position.x = x;
+    food.position.y = y;
+    food.active = true;
+}
+
+bool collision(Point p) {
+    if (p.x <= 0 || p.x >= WIDTH - 1 || p.y <= 0 || p.y >= HEIGHT - 1) {
+        return true;
+    }
+
+    for (int i = 0; i < snake.length; i++) {
+        if (snake.body[i].x == p.x && snake.body[i].y == p.y) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void game_over() {
+    mvprintw(HEIGHT / 2, WIDTH / 2 - 5, "Game Over!");
+    mvprintw(HEIGHT / 2 + 1, WIDTH / 2 - 8, "Press any key to quit");
+    refresh();
+    getch();
+    running = false;
+}
+
+void cleanup() {
+    endwin();
+}
